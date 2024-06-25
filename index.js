ddd65b5a017d062220efd2d2cea575a74a541aa3
Toni Cortes <tonicorts1984@gmail.com>
2024-06-25 20:20:21 +0200

    Added the search bar and the room objects

---
+++ index.js
@@ -0,0 +1,74 @@
+const rooms = [
+    // Granada Rooms
+    {
+        location: "Granada",
+        numberOfPeople: 2,
+        img: "img/granada_room1.jpg",
+        bedType: "double",
+        bathroom: "private"
+    },
+    {
+        location: "Granada",
+        numberOfPeople: 1,
+        img: "img/granada_room2.jpg",
+        bedType: "simple",
+        bathroom: "public"
+    },
+    {
+        location: "Granada",
+        numberOfPeople: 3,
+        img: "img/granada_room3.jpg",
+        bedType: "double",
+        bathroom: "private"
+    },
+    {
+        location: "Granada",
+        numberOfPeople: 1,
+        img: "img/granada_room4.jpg",
+        bedType: "simple",
+        bathroom: "public"
+    },
+    {
+        location: "Granada",
+        numberOfPeople: 2,
+        img: "img/granada_room5.jpg",
+        bedType: "simple",
+        bathroom: "private"
+    },
+    // Bilbao Rooms
+    {
+        location: "Bilbao",
+        numberOfPeople: 3,
+        img: "img/bilbao_room1.jpg",
+        bedType: "double",
+        bathroom: "private"
+    },
+    {
+        location: "Bilbao",
+        numberOfPeople: 2,
+        img: "img/bilbao_room2.jpg",
+        bedType: "simple",
+        bathroom: "public"
+    },
+    {
+        location: "Bilbao",
+        numberOfPeople: 1,
+        img: "img/bilbao_room3.jpg",
+        bedType: "simple",
+        bathroom: "private"
+    },
+    {
+        location: "Bilbao",
+        numberOfPeople: 2,
+        img: "img/bilbao_room4.jpg",
+        bedType: "double",
+        bathroom: "public"
+    },
+    {
+        location: "Bilbao",
+        numberOfPeople: 3,
+        img: "img/bilbao_room5.jpg",
+        bedType: "simple",
+        bathroom: "private"
+    }
+];
